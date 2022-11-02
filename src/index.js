33798db45c704b49110849f5bb8ff8f70656415f
lukitasojeda <lukitas.ojeda.00@gmail.com>
2022-11-01 21:57:35 -0300

    Implementación base de Redux Toolkit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import { AppRouter } from './router';
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { AppRouter } from "./router";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   </React.StrictMode>
 );
