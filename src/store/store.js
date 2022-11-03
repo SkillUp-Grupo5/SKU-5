b7f00c321159047da5166d10efe7ee9d92d7f362
lukitasojeda <lukitas.ojeda.00@gmail.com>
2022-11-03 01:24:12 -0300

    slices balanceSlice ands operationsSlice created and configurated on store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { authSlice } from './slices/authSlice';
+import { balanceSlice } from './slices/balanceSlice';
+import { operationsSlice } from './slices/operationsSlice';
 
 export const store = configureStore({
   reducer: {
     
       auth: authSlice.reducer,
+      balance: balanceSlice.reducer,
+      operations: operationsSlice.reducer,
   },
 })
