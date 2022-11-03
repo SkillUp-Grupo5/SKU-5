496f4b0f50a0906cbba77cfa8981dd99a11d33c3
valentin-ruggieri <valeru.251@gmail.com>
2022-11-03 19:28:15 -0300

    create view balance and forms for charge/expense

--- src/application/utils/Buttom.jsx
+++ src/application/utils/Buttom.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Button } from "@mui/material";
 
 const Buttom = (props) => {
-  const { color, variant, text, funct, href } = props;
+  const { color, variant, text, funct, href, size, width } = props;
   return (
     <Button
+      style={{ borderRadius: 10, width: width }}
+      size={size}
       color={color}
       variant={variant}
       href={href}
