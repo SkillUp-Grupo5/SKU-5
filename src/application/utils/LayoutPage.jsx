84902958aaa48ab04877cc739f1bffdcdaa59b98
valentin-ruggieri <valeru.251@gmail.com>
2022-11-03 21:23:05 -0300

    fix to bug

--- src/application/utils/LayoutPage.jsx
+++ src/application/utils/LayoutPage.jsx
@@ -1,22 +1,13 @@
-import { Box } from '@mui/system'
-import React from 'react'
-import Footer from './Footer'
-import { Navbar } from './Navbar'
+import { Box } from "@mui/system";
+import React from "react";
+import Footer from "./Footer";
+import { Navbar } from "./Navbar";
 
 export const LayoutPage = ({ children }) => {
-<<<<<<< HEAD
   return (
     <Box minWidth="100vh" bgcolor="#fff">
-      {children}
+      <Navbar>{children}</Navbar>
+      <Footer />
     </Box>
   );
 };
-=======
-	return (
-		<Box minWidth="100vh" bgcolor="#fff">
-			<Navbar>{children}</Navbar>
-			<Footer />
-		</Box>
-	)
-}
->>>>>>> 1187c67d95e5a7c817b9a0af41dcb9649a624a82
