a4bd1dd5cfbf1d549300bd53434f4f3832ce4b9b
Santi <santiagopacini@gmail.com>
2022-11-05 11:59:25 -0300

    home table  already finished

--- src/application/home/pages/Home.jsx
+++ src/application/home/pages/Home.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../styles/globals.css';
 import '../../../styles/application/home/pages/Home.css';
 import { UsersTable } from '../../utils';
 import { Box, Container } from '@mui/material';
 import { AccountActions } from '../components/AccountActions';
 import { TableHome } from '../components/TableHome';
+import { axiosClientToken } from '../../../helpers/axiosHelper';
 
 const Home = () => {
+  const [transactions, setTransactions] = useState([]);
+  const getTransactions = async () => {
+    try {
+      const {
+        data: { data },
+      } = await axiosClientToken.get('/transactions');
+      setTransactions(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+  console.log(transactions);
   return (
     <div className="App">
       <Box
@@ -19,7 +36,7 @@ const Home = () => {
           <Box height="90vh" padding={4}>
             <Box width="100%">
               <AccountActions />
-              <TableHome />
+              <TableHome transactions={transactions} />
             </Box>
           </Box>
         </Container>
