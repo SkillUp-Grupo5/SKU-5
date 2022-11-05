0f255feb9edee4d50f75942292504418ca19d04c
valentin-ruggieri <valeru.251@gmail.com>
2022-11-05 19:06:16 -0300

    fix to bugs

--- src/api/balance.js
+++ src/api/balance.js
@@ -1,37 +1,41 @@
-import { axiosClientToken } from '../helpers'
+import { axiosClientToken } from "../helpers";
 
 export const addDeposit = async (data) => {
-	try {
-		const deposit = await axiosClientToken.post('/transactions', { ...data })
-		return deposit
-	} catch (error) {
-		console.log(error)
-	}
-}
+  try {
+    if (data.userId) {
+      const deposit = await axiosClientToken.post("/transactions", data );
+      return deposit;
+    } else {
+      console.log("no hay iduser");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const deleteDeposit = async () => {
-	try {
-		const deposit = await axiosClientToken.delete(`/transactions/${286}`)
-		return deposit
-	} catch (error) {
-		console.log(error)
-	}
-}
+  try {
+    const deposit = await axiosClientToken.delete(`/transactions/${286}`);
+    return deposit;
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const showDeposits = async () => {
-	try {
-		const { data } = await axiosClientToken.get('/transactions')
-		const res = data.data.map((e) => ({ amount: e.amount, type: e.type }))
-		const charges = res
-			.filter((e) => e.type == 'topup')
-			.map((e) => Number(e.amount))
-			.reduce((a, b) => a + b, 0)
-		const expenses = res
-			.filter((e) => e.type == 'payload')
-			.map((e) => Number(e.amount))
-			.reduce((a, b) => a + b, 0)
+  try {
+    const { data } = await axiosClientToken.get("/transactions");
+    const res = data.data.map((e) => ({ amount: e.amount, type: e.type }));
+    const charges = res
+      .filter((e) => e.type == "topup")
+      .map((e) => Number(e.amount))
+      .reduce((a, b) => a + b, 0);
+    const expenses = res
+      .filter((e) => e.type == "payment")
+      .map((e) => Number(e.amount))
+      .reduce((a, b) => a + b, 0);
 
-		return { charges, expenses }
-	} catch (error) {
-		console.log(error)
-	}
-}
+    return { charges, expenses };
+  } catch (error) {
+    console.log(error);
+  }
+};
